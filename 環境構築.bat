0636a6e8f229b544896e871c1867a97561a08107
kero <kero@DESKTOP-V8BK7SI.localdomain>
2020-08-22 14:57:45 +0900

    環境構築用

---
+++ 環境構築.bat
@@ -0,0 +1,7 @@
+mkdir api\src\main\resources
+mkdir api\src\test\resources
+mkdir common\src\main\resources
+mkdir network\src\main\resources
+mkdir network\src\test\resources
+mkdir ui\src\main\resources
+mkdir ui\src\test\resources
